f4c66a263ee9e27bd17d940a53a45334b307424d
kmmac89 <kmmac89@gmail.com>
2020-01-05 20:23:47 -0500

    1_5_2020 Attempt 1 at JS rock-paper-scissors game

---
+++ script.js
@@ -0,0 +1,35 @@
+let a = "rock";
+let b = "paper";
+let c = "scissors";
+
+function computerPlay() {
+	let computerRoll = Math.floor(Math.random() * 3); 
+	if (computerRoll === 0)
+		return a;
+	else if (computerRoll === 1)
+		return b;
+	else
+		return c;
+}
+
+const computerSelection = computerPlay ();
+
+const playerSelection = prompt ();
+
+function playRound(playerSelection, computerSelection) {
+if (playerSelection === a && computerSelection === a);
+	alert ("tie")
+	else if (playerSelection === a && computerSelection === b);
+	alert ("computer win")
+	else ( playerSelection === a && computerSelection === c);
+	alert ("player win")
+
+
+
+func game()
+
+let counter = 1;
+let a = ++counter;
+
+console.log()
+
