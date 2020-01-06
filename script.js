5744ef460f3d5204e71d9e18876ffe6c3dc38463
kris <59074910+kmmac89@users.noreply.github.com>
2020-01-06 15:17:19 -0500

    Adding variables, let/if/else still wip

--- script.js
+++ script.js
@@ -4,27 +4,41 @@ let c = "scissors";
 
 function computerPlay() {
 	let computerRoll = Math.floor(Math.random() * 3); 
-	if (computerRoll === 0)
+	if (computerRoll === 0) 
 		return a;
 	else if (computerRoll === 1)
 		return b;
 	else
-		return c;
+		return c; 
 }
 
 const computerSelection = computerPlay ();
 
 const playerSelection = prompt ();
 
-function playRound(playerSelection, computerSelection);
-	let roundWinner = ();
-	if (playerSelection === a && computerSelection === a) {
-		alert ("tie");
-		else if (playerSelection === a && computerSelection === b)
-		alert ("computer win");
-		else ( playerSelection === a && computerSelection === c)
-		alert ("player win");
+
+
+
+function playRound(playerSelection, computerSelection) {
+	let result = (playerSelection === a && computerSelection === b)
+	if (result = true);
+		return (++computerWin);
+		alert ("Computer wins!"); }
+	
+	
+	
+	
+	
+	
+	let computerWin = 0;
+	let playerWin = 0;
+	if (playerSelection === a && computerSelection === b) 
+		return (++computerWin);
+		alert ("Computer wins!");
+		
+	
 }
+
 
 
 func game()
