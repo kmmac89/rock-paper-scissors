e7e52053ed231841168d97f67c3ca9b81e729067
kmmac89 <kmmac89@gmail.com>
2020-12-07 12:22:26 -0500

    Restructured variables and functions

--- script.js
+++ script.js
@@ -1,50 +1,80 @@
-let a = "rock";
-let b = "paper";
-let c = "scissors";
 
-function computerPlay() {
-	let computerRoll = Math.floor(Math.random() * 3); 
-	if (computerRoll === 0) 
-		return a;
-	else if (computerRoll === 1)
-		return b;
-	else
-		return c; 
+/* Rock Paper Scissors is a game in which two players make a selection from three pre-determined, weighted options.
+By luck of the draw, the player who chooses the higher-weighted options wins the round. Players will compete five times to determine the winner of the game. */
+
+let playerSelection = prompt("...").toLowerCase();
+let computerSelection = computerPlay ();
+	function computerPlay() {
+		let computerRoll = Math.floor(Math.random() * 3); 
+			if (computerRoll === 0) {
+				return "rock";
+			} else if (computerRoll === 1) {
+				return "paper";
+			} else {
+				return "scissors"; 
+			}
+		}
+			
+let computerScore = 0;
+let playerScore = 0;
+let gameRound = 0;
+
+
+
+
+let playGame = playRound ();
+	function playRound(playerSelection, computerSelection) { 
+		if ((playerSelection == "rock" && computerSelection == "scissors") ||
+			(playerSelection == "scissors" && computerSelection == "paper") ||
+			(playerSelection == "paper" && computerSelection == "rock")) {
+				return playerScore = playerScore + 1;
+		gameRound++;
+		console.log("player wings this roung owo");
+		}	else if (playerSelection === computerSelection) {
+		/* WELL FELLAS WE GOT DOWN TO THIS LINE WOAH BOY GOTTA SAY I AM PROUD OF THIS TEAM !!! FERDA */
+	 	console.log("ohhh no a tie try again!");
+		}	else {
+				return computerScore = computerScore + 1;
+		gameRound++;
+		console.log("compupter wings!! yayy!");
+	}
+
+
+
+return computerScore;
+	if (computerScore == 5) { 
+		console.log("game over")}
+
+return playerScore;
+	if (playerScore == 5) { 
+		console.log("game over")}
+
+return gameRound;
+	if (gameRound == 5) {
+		console.log("game over")
+	}
 }
 
-const computerSelection = computerPlay ();
 
-const playerSelection = prompt ();
-
-
+/* junk
+const 
 
 
-function playRound(playerSelection, computerSelection) {
-	let result = (playerSelection === a && computerSelection === b)
-	if (result = true);
-		return (++computerWin);
-		alert ("Computer wins!"); }
-	
-	
-	
-	
-	
-	
-	let computerWin = 0;
-	let playerWin = 0;
-	if (playerSelection === a && computerSelection === b) 
-		return (++computerWin);
-		alert ("Computer wins!");
-		
-	
-}
+computerPlay ();
+playerSelection ();
+
+playRound ();
 
 
+let 
 
-func game()
+for (i = 0; i < 5; i++) {
 
-let counter = 1;
-let a = ++counter;
+function playGame () {
+	loop (playRound; gameRound = 5 {
+		if ()
+	}
 
-console.log()
+playGame ();
 
+*/
